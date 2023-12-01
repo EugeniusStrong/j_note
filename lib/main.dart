39e9a23e38b5336324e29540dbb9d8ad24c5431d
EugeniusStrong <EugeniusStrong@gmail.com>
2023-12-01 21:08:33 +0500

    Added card widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:j_note/auth_switch/login_switch.dart';
 import 'package:j_note/firebase_options.dart';
+import 'package:j_note/widgets/task_widget.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: const LoginSwitch(),
+      home: const TaskWidget(),
     );
   }
 }
